9b2d515fb37993db9b57e6acbaecfbf1f309a22b
jantapabh <jantapa2407@gmail.com>
2020-02-11 21:11:31 +0700

    test commit server

---
+++ client.js
@@ -0,0 +1,36 @@
+// TCP Client
+
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 1337;
+let i = 1;
+
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function () {
+
+    console.log('Connected');
+    client.write('+'); // + or -
+
+});
+
+client.on('data', function (data) {
+
+    var resp = ("" + data).trim();
+    if(resp == "OK")
+    {
+        client.write('cat');
+    }
+  
+
+});
+
+client.on('error', function (error) {
+    // console.log(error);
+});
+
+
+// Add a 'close' event handler for the client socket
+client.on('close', function () {
+    console.log('Connection closed');
+});
--- server.js
+++ server.js
@@ -1,5 +1,48 @@
-//Server
-
 
 var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 1337;
+var j = 1;
+var i = 1;
 
+
+
+net.createServer(function (sock) {
+
+
+
+    console.log('Strat connect : ' + sock.remoteAddress + ':' + sock.remotePort);
+
+
+
+    sock.on('data', function (data) {
+
+        if(data.toString() == "+")
+        {
+                sock.write("OK");
+
+        }
+        else{
+
+            sock.write("NO");
+        }
+
+      
+
+       
+    });
+
+    sock.on('close', function (data) {
+
+        // sock.destroy();
+
+    });
+
+    sock.on('error', function (error) {
+        // console.log(error);
+    });
+
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST + ':' + PORT);
