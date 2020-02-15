764d75775fb78bf08c1f9a8e0374e045c35a8fe3
jantapabh <jantapa2407@gmail.com>
2020-02-15 23:11:11 +0700

    test commit

---
+++ 58/client1.js
@@ -0,0 +1,41 @@
+// TCP Client
+
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 1337;
+let i = 1;
+let j = 0;
+
+
+
+
+var client = new net.Socket();
+
+client.connect(PORT, HOST, function () {
+
+    console.log('Connected');
+    client.write("+"); // + or -
+
+});
+
+client.on('data', function (data) {
+
+    var resp = data.toString().trim();
+
+    if(resp.toString() == "OK")
+    {
+        client.write('bird');
+       
+       
+    }
+});
+
+client.on('error', function (error) {
+    // console.log(error);
+});
+
+
+// Add a 'close' event handler for the client socket
+client.on('close', function () {
+    console.log('Connection closed');
+});
