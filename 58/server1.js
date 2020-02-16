619f78b92cf9759306f1de9f0474b7e0b287d90c
jantapabh <jantapa2407@gmail.com>
2020-02-16 23:52:27 +0700

    1 2 3

--- 58/server1.js
+++ 58/server1.js
@@ -6,10 +6,6 @@ var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 1337;
 var j = 0;
-let countCat = 0;
-let countDog = 0;
-let countBird = 0;
-
 
 
 net.createServer(function (sock) {
@@ -19,64 +15,7 @@ net.createServer(function (sock) {
     sock.on('data', function (data) {
 
 
-        if (data.toString() == "+" || data.toString() == "-") {
-
-            sock.write("OK");
-
-            console.log(data.toString()) //แสดงเครื่องหมาย
-
-            if (data.toString() == "+") {
-
-                j = 1;
-
-            }
-            else {
-
-                j = 0;
-            }
-
-        }
-
-        else {
-
-            if (j == 1) {
-
-                if (data.toString() == 'cat') {
-                    countCat = countCat + 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-                else if (data.toString() == 'dog') {
-                    countDog = countDog + 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-                else {
-                    countBird = countBird + 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-            }
-            else {
-
-                if (data.toString() == 'cat') {
-                    countCat = countCat - 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-                else if (data.toString() == 'dog') {
-                    countDog = countDog - 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-                else {
-                    countBird = countBird - 1;
-                    console.log({ cat: +countCat, dog: +countDog, bird: +countBird });
-                    sock.destroy();
-                }
-
-            }
-        }
+    
 
     }
 
